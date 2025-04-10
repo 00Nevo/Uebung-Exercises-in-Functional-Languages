1aa8372730ea805921abd518d8e43c788a155b63
NEVO <albertoborgato9@gmail.com>
2025-04-10 20:33:24 +0200

    Create Ch2.clj

---
+++ Ch2.clj
@@ -0,0 +1,29 @@
+Chapter 2
+How to Use Emacs, an Excellent Clojure Editor
+
+Installation
+Configuration
+Emacs Escape Hatch
+Emacs Buffers
+Working with Files
+Key Bindings and Modes
+Emacs Is a Lisp Interpreter
+Modes
+Installing Packages
+Core Editing Terminology and Key Bindings
+Point
+Movement
+Selection with Regions
+Killing and the Kill Ring
+Editing and Help
+Using Emacs with Clojure
+Fire Up Your REPL!
+Interlude: Emacs Windows and Frames
+A Cornucopia of Useful Key Bindings
+How to Handle Errors
+Paredit
+Wrapping and Slurping
+Barfing
+Navigation
+Continue Learning
+Summary
